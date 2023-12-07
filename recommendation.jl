a9f4961a732886455d594092b31e7c8125d344f0
Kevin Arfi <arfy.kevin@gmail.com>
2023-12-07 18:16:12 +0100

    samples from the L-ensemble and create a csv result file.

---
+++ recommendation.jl
@@ -0,0 +1,48 @@
+using LinearAlgebra
+
+using DataFrames
+using CSV
+
+using Determinantal 
+
+####Data set up
+
+#the tournesol score is not included in the criteria
+CRITERIA = [
+    "reliability",
+    "importance",
+    "engaging",
+    "pedagogy",
+    "layman_friendly",
+    "entertaining_relaxing",
+    "better_habits",
+    "diversity_inclusion",
+    "backfire_risk",
+]
+
+df = DataFrame(CSV.File("tournesol_scores_above_20_2023-09-18.csv"))
+
+tournesol_scores = Diagonal(coalesce.(df[:, :tournesol_score], 0))
+
+diversity_scores = Matrix(coalesce.(df[:, CRITERIA], 0))
+
+X = tournesol_scores*diversity_scores
+
+#Set up the L-Ensemble
+K = LowRank(X)
+
+L = EllEnsemble(K)
+
+#Sample
+n_sample = 10
+
+bundle_size = 9
+
+results = Array{Int64, 2}(undef, n_sample, bundle_size)
+
+for i in 1:n_sample
+	results[i,:] = sample(L, bundle_size)
+end
+
+CSV.write("dpp_sampling.csv",DataFrame(results, :auto))
+
