f32f506b8f24af0e5c004e012bf61f3b97877c41
Kevin Arfi <arfy.kevin@gmail.com>
2023-12-13 23:25:41 +0100

    Linear quality model with tournesol term + criteria term + recency term.

--- recommendation.jl
+++ recommendation.jl
@@ -1,13 +1,11 @@
 using LinearAlgebra
+using Dates
 
 using DataFrames
 using CSV
 
 using Determinantal 
 
-####Data set up
-
-#the tournesol score is not included in the criteria
 CRITERIA = [
     "reliability",
     "importance",
@@ -18,15 +16,55 @@ CRITERIA = [
     "better_habits",
     "diversity_inclusion",
     "backfire_risk",
-]
+]#the tournesol score is not included in the criteria
 
+#Quality Model
+function get_age_in_days(date::AbstractString, ref_date::Date)::Int
+	return max((ref_date - Date(date[1:10], dateformat"yyyy-mm-dd")).value, 1)
+	end
+
+function compute_qualities(
+			   criteria_coefficient::Number, 
+			   recency_coefficient::Number, 
+			   tournesol_scores::Array{Float64, 1}, 
+			   criteria_scores::Array{Float64, 2}, 
+			   ages_in_day::Array{Int64, 1}
+			   )::Array{Float64, 1}
+	return tournesol_scores + criteria_coefficient*vec(sum(max.(criteria_scores, 0), dims=2)) + recency_coefficient./ages_in_day
+	end
+
+ref_date = Date("2023-12-13", dateformat"yyyy-mm-dd")
+
+#Data set up
 df = DataFrame(CSV.File("tournesol_scores_above_20_2023-09-18.csv"))
 
-tournesol_scores = Diagonal(coalesce.(df[:, :tournesol_score], 0))
+tournesol_scores = Vector(coalesce.(df[:, :tournesol_score], 0))
 
-diversity_scores = Matrix(coalesce.(df[:, CRITERIA], 0))
+criteria_scores = Matrix(coalesce.(df[:, CRITERIA], 0))
 
-X = tournesol_scores*diversity_scores
+ages_in_days = get_age_in_days.(df[:, :publication_date], ref_date)
+
+coefficients = DataFrame(CSV.File("quality_function_coefficient.csv"))
+criteria_coefficients = coefficients[:,:criteria_coefficient]
+recency_coefficients = coefficients[:,:recency_coefficient]
+
+qualities = compute_qualities(
+			      criteria_coefficients[1],
+			      recency_coefficients[1],
+			      tournesol_scores,
+			      criteria_scores,
+			      ages_in_days
+			      )
+#Norm each line of criteria score
+criteria_scores_norms = sqrt.(sum(abs2, criteria_scores, dims=2)) 
+normed_criteria_scores = criteria_scores
+for i in 1:size(criteria_scores)[1]
+	if criteria_scores_norms[i] > 0.0
+		normed_criteria_scores[i,:] /= criteria_scores_norms[i]
+	end
+end
+
+X = Diagonal(qualities)*normed_criteria_scores
 
 #Set up the L-Ensemble
 K = LowRank(X)
@@ -46,4 +84,3 @@ for i in 1:n_sample
 end
 
 CSV.write("dpp_sampling_bundle_size="*string(bundle_size)*"_n_sample="*string(n_sample)*".csv",DataFrame(results, :auto))
-
