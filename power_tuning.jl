a7cfd2a2ca4f43b3c06e80764244ced5b5223991
Kevin Arfi <arfy.kevin@gmail.com>
2024-02-10 11:00:05 +0100

    sample for each power and gathers the means of top5% and bottom 50% in an array.

--- power_tuning.jl
+++ power_tuning.jl
@@ -8,17 +8,33 @@ using Plots
 
 using Determinantal 
 
-
-include("utils.jl")
-
+CRITERIA = [
+    "reliability",
+    "importance",
+    "engaging",
+    "pedagogy",
+    "layman_friendly",
+    "entertaining_relaxing",
+    "better_habits",
+    "diversity_inclusion",
+    "backfire_risk",
+]#the tournesol score is not included in the criteria
 
 #Data set up
 df = DataFrame(CSV.File("tournesol_scores_above_20_2023-09-18.csv"))
+
+criteria_scores = Matrix(coalesce.(df[:, CRITERIA], 0))
+
 tournesol_scores = Vector(coalesce.(df[:, :tournesol_score], 0))
 quantile_95 = quantile!(tournesol_scores, 0.95)
 quantile_50 = quantile!(tournesol_scores, 0.50)
 
 tournesol_scores_powers = range(1, 10)
+
+results = Array{Number, 2}(undef, length(tournesol_scores_powers), 3)
+results[:,1] = tournesol_scores_powers
+#Second column will contain the average number of videos from the top 5%
+#Third column will contain the average number of videos from the bottom 50%
 
 for power in tournesol_scores_powers
 	#Quality model
@@ -41,13 +57,18 @@ for power in tournesol_scores_powers
 	#Sample
 	sample_size = 1000
 	bundle_size = 9
-	results = Array{Number, 2}(undef, sample_size, 2)
+	counts = Array{Number, 2}(undef, sample_size, 2)
 
 	for i in 1:sample_size
 		indexes = sample(L, bundle_size)
 		top_5percent_count = sum(tournesol_scores[indexes].>=quantile_95)
 		bottom_50percent_count = sum(tournesol_scores[indexes].<=quantile_50)
-		results[i,:] = [top_5percent_count, bottom_50percent_count]
+		counts[i,:] = [top_5percent_count, bottom_50percent_count]
 	end
+
+	results[power,2:3] = mean(counts, dims=1)
+end
+
+#Plots 
 
 
