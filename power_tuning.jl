4f4d4465ee443cb0f4ab8db82213a952d0d210f9
Kevin Arfi <arfy.kevin@gmail.com>
2024-02-10 10:35:06 +0100

    add the sampling part

--- power_tuning.jl
+++ power_tuning.jl
@@ -15,6 +15,8 @@ include("utils.jl")
 #Data set up
 df = DataFrame(CSV.File("tournesol_scores_above_20_2023-09-18.csv"))
 tournesol_scores = Vector(coalesce.(df[:, :tournesol_score], 0))
+quantile_95 = quantile!(tournesol_scores, 0.95)
+quantile_50 = quantile!(tournesol_scores, 0.50)
 
 tournesol_scores_powers = range(1, 10)
 
@@ -35,5 +37,17 @@ for power in tournesol_scores_powers
 	X = Diagonal(qualities)*normed_criteria_scores
 	K = LowRank(X)
 	L =  EllEnsemble(K)
+
+	#Sample
+	sample_size = 1000
+	bundle_size = 9
+	results = Array{Number, 2}(undef, sample_size, 2)
+
+	for i in 1:sample_size
+		indexes = sample(L, bundle_size)
+		top_5percent_count = sum(tournesol_scores[indexes].>=quantile_95)
+		bottom_50percent_count = sum(tournesol_scores[indexes].<=quantile_50)
+		results[i,:] = [top_5percent_count, bottom_50percent_count]
+	end
 
 
