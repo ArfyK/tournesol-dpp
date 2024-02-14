9f31695218f07469fd845617ea7bbc429ce29f3e
Kevin Arfi <arfy.kevin@gmail.com>
2024-02-14 15:46:05 +0100

    add plot_general_results

--- utils.jl
+++ utils.jl
@@ -3,6 +3,59 @@ using CSV
 using Plots
 
 #Plots
+function plot_general_results(df, discount_range, caracteristic_time_range, power_range)
+	p1 = plot(df[:, :maximum_selection_frequency],
+		  df[:,:prop_top_20_month], 
+	          seriestype=:scatter,
+	          ylabel="Top 20 proportion",
+	          xlabel="Maximum selection frequency",
+	          legend=false
+	)
+	p2 = plot(df[:, :discount],
+		  df[:,:prop_top_20_month], 
+	          seriestype=:scatter,
+	          ylabel="Top 20 proportion",
+	          xlabel="Discount",
+	          legend=false,
+		  mc=:red
+	)
+	p3 = plot(df[:, :caracteristic_time],
+		  df[:,:prop_top_20_month], 
+	          seriestype=:scatter,
+	          ylabel="Top 20 proportion",
+	          xlabel="Caracteristic time",
+	          legend=false,
+		  mc=:green
+	)
+	p4 = plot(df[:, :power],
+		  df[:,:prop_top_20_month], 
+	          seriestype=:scatter,
+	          ylabel="Top 20 proportion",
+	          xlabel="Power",
+	          legend=false,
+		  mc=:grey
+	)
+	plot(p1, 
+	     p2, 
+	     p3, 
+	     p4,
+	     layout=(2,2), 
+	     grid=true,
+	     size=(1080, 720),
+	     plot_title="Discount in ["*string(Float64(discount_range.ref))*","*string(Float64(discount_range.ref+(discount_range.len-1)*discount_range.step))*"] "*
+			"Caracteristic times in ["*string(Float64(caracteristic_time_range.ref))*","*string(Float64(caracteristic_time_range.ref+(caracteristic_time_range.len-1)*caracteristic_time_range.step))*"]\n"*
+			"power_range_["*string(Float64(power_range.ref))*","*string(Float64(power_range.ref+(power_range.len-1)*power_range.step))*"]"
+	)
+
+
+	savefig("recency_tuning/General_results_"*
+	"discount_["*string(Float64(discount_range.ref))*","*string(Float64(discount_range.ref+(discount_range.len-1)*discount_range.step))*"]_"*
+	"caracteristic_time_range_["*string(Float64(caracteristic_time_range.ref))*","*string(Float64(caracteristic_time_range.ref+(caracteristic_time_range.len-1)*caracteristic_time_range.step))*"]_"*
+	"power_range_["*string(Float64(power_range.ref))*","*string(Float64(power_range.ref+(power_range.len-1)*power_range.step))*
+	  "].png"
+	 )
+end
+
 function plot_parameters_sets_details(
 			      df,
 			      parameters_sets
