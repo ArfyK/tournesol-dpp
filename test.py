a52a74f7bbc2e60f2b136b92977f8d7e24317f76
Kevin Arfi <arfy.kevin@gmail.com>
2024-02-27 16:59:45 +0100

    test create_children

--- test.py
+++ test.py
@@ -4,9 +4,24 @@ from symmetric_polynomials import Node
 
 n = 3
 indexes = list(range(n))
-eigenvalues = np.random.default_rng().normal(10, 10,n)
+eigenvalues = np.random.default_rng().normal(10, 10, n)
 
 node = Node(indexes=indexes, eigenvalues=eigenvalues)
 
-print(node)
+print(node.indexes)
+print(node.eigenvalues)
+print(node.right_child)
+print(node.left_child)
 
+node.create_children(node.eigenvalues)
+print(node.right_child.indexes)
+print(node.right_child.eigenvalues)
+
+print(node.left_child.indexes)
+print(node.left_child.eigenvalues)
+
+print(node.left_child.right_child.indexes)
+print(node.left_child.right_child.eigenvalues)
+
+print(node.left_child.left_child.indexes)
+print(node.left_child.left_child.eigenvalues)
