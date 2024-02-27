b349af908011fc236016edff635d2921abf7e76c
Kevin Arfi <arfy.kevin@gmail.com>
2024-02-27 16:59:34 +0100

    create_children

--- symmetric_polynomials.py
+++ symmetric_polynomials.py
@@ -1,12 +1,40 @@
-#Implements the algorithm describe in "DPP for Machine Learning" in section 5.2.3 computing the singleton marginals of a k-dpp
+# Implements the algorithm describe in "DPP for Machine Learning" in section 5.2.3 computing the singleton marginals of a k-dpp
 
 import numpy as np
 
+
 class Node:
-    def __init__(self, indexes=None, eigen_values=None, symmetric_polynomials=None, left_child=None, right_child=None):
+    def __init__(
+        self,
+        indexes=None,
+        eigenvalues=None,
+        symmetric_polynomials=None,
+        left_child=None,
+        right_child=None,
+    ):
         self.indexes = indexes
-        self.eigen_values = eigen_values
+        self.eigenvalues = eigenvalues
         self.symmetric_polynomials = symmetric_polynomials
         self.left_child = left_child
         self.right_child = right_child
 
+    def create_children(self, eigenvalues):
+        n = len(self.indexes)
+        if n > 1:
+            self.right_child = Node(
+                indexes=self.indexes[: n // 2],
+                eigenvalues=eigenvalues[self.indexes[: n // 2]],
+            )
+            self.right_child.create_children(eigenvalues)
+            self.left_child = Node(
+                indexes=self.indexes[n // 2 :],
+                eigenvalues=eigenvalues[self.indexes[n // 2 :]],
+            )
+            self.left_child.create_children(eigenvalues)
+
+
+class BinaryTree:
+    def __init__(self, eigenvalues):
+        self.root = Node(eigenvalues=eigenvalues, indexes=list(range(eigenvalues.size)))
+        current_node = self.root
+        current_indexes = list(range(eigenvalues))
