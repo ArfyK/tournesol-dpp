8482ce48d6207240a2ba96188e293631c38fe06e
Kevin Arfi <arfy.kevin@gmail.com>
2024-03-06 19:04:57 +0100

    test build_dataset

--- dataset_tests.py
+++ dataset_tests.py
@@ -2,12 +2,10 @@ import pandas as pd
 
 import dataset
 
-collective_criteria_scores = pd.read_csv('tournesol_dataset/collective_criteria_scores.csv')
+collective_criteria_scores = pd.read_csv(
+    "tournesol_dataset/collective_criteria_scores.csv"
+)
 
-video_ids = collective_criteria_scores['video'].unique()
-n_videos = 53 
-small_collective_criteria_scores = collective_criteria_scores.loc[collective_criteria_scores['video'].isin(video_ids[:n_videos])]
+df = dataset.build_dataset(collective_criteria_scores)
 
-df, r = dataset.build_dataset(small_collective_criteria_scores, tournesol_score_threshold=-100)
-
-print(r.json())
+print(df["publication_date"])
