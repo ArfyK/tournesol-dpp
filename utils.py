42f779ad35f0af742f5a513e10ad3adf16c5e094
Kevin Arfi <arfy.kevin@gmail.com>
2024-03-13 15:42:13 +0100

    ages_in_days handle the nan case

--- utils.py
+++ utils.py
@@ -13,16 +13,18 @@ CRITERIA = [
     "backfire_risk",
 ]
 
+
 def get_age_in_days(video_series, ref_date):
+    # handle the nan case
+    if type(video_series["publication_date"]) == type(0.1):
+        return
     # return 1 if the video is less than a day old
     return max(
         (
             ref_date
             - datetime.datetime.strptime(
-                video_series["publication_date"].split("T")[0], "%Y-%m-%d"
+                video_series["publication_date"], "%Y-%m-%dT%H:%M:%SZ"
             )
         ).days,
         1,
     )  # remove the time part of the datetime with the split because some entries only have the date part.
-
-
