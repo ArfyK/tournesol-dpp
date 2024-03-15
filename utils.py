d59743434e20ef0d14955a09ca534a8936e9135b
Kevin Arfi <arfy.kevin@gmail.com>
2024-03-15 10:20:26 +0100

    handle nan publication dates

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import datetime
 
 CRITERIA = [
@@ -15,16 +16,16 @@ CRITERIA = [
 
 
 def get_age_in_days(video_series, ref_date):
-    # handle the nan case
-    if type(video_series["publication_date"]) == type(0.1):
-        return
     # return 1 if the video is less than a day old
-    return max(
-        (
-            ref_date
-            - datetime.datetime.strptime(
-                video_series["publication_date"], "%Y-%m-%dT%H:%M:%SZ"
-            )
-        ).days,
-        1,
-    )  # remove the time part of the datetime with the split because some entries only have the date part.
+    try:
+        return max(
+            (
+                ref_date
+                - datetime.datetime.strptime(
+                    video_series["publication_date"], "%Y-%m-%dT%H:%M:%SZ"
+                )
+            ).days,
+            1,
+        )
+    except TypeError:
+        return np.nan
