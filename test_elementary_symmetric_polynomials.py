1e2941e402b0b4d98c8b9200aebb4a3c40331184
Kevin Arfi <arfy.kevin@gmail.com>
2024-03-16 21:48:57 +0100

    adapt tests to the deletion on binary_tree class

--- test_elementary_symmetric_polynomials.py
+++ test_elementary_symmetric_polynomials.py
@@ -3,7 +3,7 @@ import copy
 import numpy as np
 
 from dppy.exact_sampling import elementary_symmetric_polynomials
-from elementary_symmetric_polynomials import Node, BinaryTree
+from elementary_symmetric_polynomials import Node
 
 n = 3
 indexes = list(range(n))
@@ -11,8 +11,7 @@ eigenvalues = np.random.default_rng().normal(10, 10, n)
 
 try:
     print("Test create_children")
-    bt = BinaryTree(eigenvalues)
-    node = bt.root
+    node = Node.from_eigen_values(eigenvalues)
 
     print(node.indexes)
     print(node.eigenvalues)
@@ -37,48 +36,44 @@ except:
 print("\n")
 
 print("Test the computation of elementary symmetric polynomials")
-bt.root.compute_elementary_symmetric_polynomials(n)
-# print(bt.root.elementary_symmetric_polynomials)
-# print(bt.root.right_child.elementary_symmetric_polynomials)
-# print(bt.root.left_child.elementary_symmetric_polynomials)
+node.compute_elementary_symmetric_polynomials(n)
 
-esp = elementary_symmetric_polynomials(bt.root.eigenvalues, n)
+esp = elementary_symmetric_polynomials(node.eigenvalues, n)
 # print(esp)
 for i in range(n + 1):
     try:
-        assert bt.root.elementary_symmetric_polynomials[i] == esp[i][n]
+        assert node.elementary_symmetric_polynomials[i] == esp[i][n]
     except AssertionError:
-        print(bt.root.elementary_symmetric_polynomials[i])
+        print(node.elementary_symmetric_polynomials[i])
         print(esp[i][n])
 
 print("\n")
 print("Test the path removing algorithm")
 for i in range(n):
     print(i)
-    bt_copy = copy.deepcopy(bt)
-    bt_copy.root.remove_path(i)
-    node = bt_copy.root
+    node_copy = copy.deepcopy(node)
+    node_copy.remove_path(i)
 
-    print(node.indexes)
-    print(node.eigenvalues)
+    print(node_copy.indexes)
+    print(node_copy.eigenvalues)
 
-    print(node.right_child.indexes)
-    print(node.right_child.eigenvalues)
+    print(node_copy.right_child.indexes)
+    print(node_copy.right_child.eigenvalues)
 
-    print(node.left_child.indexes)
-    print(node.left_child.eigenvalues)
+    print(node_copy.left_child.indexes)
+    print(node_copy.left_child.eigenvalues)
 
-print('\n')
+print("\n")
 print("Test calculation of e^{-n}_{k-1}")
 for i in range(n):
-    bt_copy = copy.deepcopy(bt)
-    bt_copy.root.remove_path(i)
-    bt_copy.root.compute_elementary_symmetric_polynomials(n - 1)
+    node_copy = copy.deepcopy(node)
+    node_copy.remove_path(i)
+    node_copy.compute_elementary_symmetric_polynomials(n - 1)
     esp = elementary_symmetric_polynomials(
-        np.concatenate((bt.root.eigenvalues[:i], bt.root.eigenvalues[i + 1 :])), n - 1
+        np.concatenate((node.eigenvalues[:i], node.eigenvalues[i + 1 :])), n - 1
     )
     try:
-        assert np.array_equal(bt_copy.root.elementary_symmetric_polynomials, esp[:,n - 1])
+        assert np.array_equal(node_copy.elementary_symmetric_polynomials, esp[:, n - 1])
     except AssertionError:
-        print(bt_copy.root.elementary_symmetric_polynomials)
-        print(esp[:,n - 1])
+        print(node_copy.elementary_symmetric_polynomials)
+        print(esp[:, n - 1])
